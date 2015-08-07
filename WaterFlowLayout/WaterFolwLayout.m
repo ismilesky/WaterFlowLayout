fc31d2a35190c0b98ce710f91601bb6bc3140ca7
ismilesky <fighter_kongfei126@.com>
2015-08-07 14:50:12 +0800

    Update WaterFolwLayout.m

--- WaterFlowLayout/WaterFolwLayout.m
+++ WaterFlowLayout/WaterFolwLayout.m
@@ -2,8 +2,7 @@
 //  WaterFolwLayout.m
 //  WaterFlowLayout
 //
-//  Created by 小爪乎黑 on 15/7/29.
-//  Copyright (c) 2015年 李帅. All rights reserved.
+
 //
 
 #import "WaterFolwLayout.h"
@@ -130,16 +129,7 @@
         
         //  更新高度
         self.columnHeights[shortesIndex] = @(detalY + itemHeight);
-        
-        
-        
     }
-    
-    
-    
-    
-
-
 }
 
 - (CGSize)collectionViewContentSize
